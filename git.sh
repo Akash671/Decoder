7a8052aeb04fd867ea6c26f310233f29d86ba9da
Akash671 <akashsaini454545gmail.com>
2021-04-18 22:05:33 +0530

    fnsjfsdifdf

--- git.sh
+++ git.sh
@@ -14,8 +14,9 @@ read -p "| Enter commit message |:|" message
 echo "$(tput setaf 10)+----------------------+ +---------------------+"
 git commit -m "$message"
 #echo "$(tput setaf 41)Your message is : $message"
+echo "+-------------------------------+++-----------------------------------+
 git push
-
+echo "+-------------------------------+++-----------------------------------+
 echo "$(tput setaf 31)+---------------------------------+"
 echo "$(tput setaf 31)|    Thank You! Akash Kumar       |"
 echo "$(tput setaf 31)+---------------------------------+"
